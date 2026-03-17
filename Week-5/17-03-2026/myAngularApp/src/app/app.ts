d320eb217f6d091270d4844dfd05826543682eff
lakshay <lakshaymangal51@gmail.com>
2026-03-17 11:35:00 +0530

    Course work for respective dates

---
+++ Week-5/17-03-2026/myAngularApp/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { GreetingComponent } from './greeting/greeting';
+import { Home } from './home/home';
+import { Product } from './product/product';
+import { User } from './user/user';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,GreetingComponent, Home,Product,User],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('myAngularApp');
+}
