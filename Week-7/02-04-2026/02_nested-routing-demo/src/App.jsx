76df8d1e61b3f59fade4b04cdc37a8d879e9694c
lakshay <lakshaymangal51@gmail.com>
2026-04-02 14:34:01 +0530

    Bugfix

--- Week-7/02-04-2026/02_nested-routing-demo/src/App.jsx
+++ Week-7/02-04-2026/02_nested-routing-demo/src/App.jsx
@@ -1,17 +1,25 @@
-import { BrowserRouter ,Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import './App.css';
 import Homepage from './Homepage';
 import About from './About';
+import Layout from './Layout';
 import Contact from './Contact';
 
-function App(){
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element= {<Layout/>}>
-          <Route index element= {<Homepage/>}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Homepage />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
