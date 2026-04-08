40b63f3efe3f13be83998f2bdf426ee1a79ae5d8
lakshay <lakshaymangal51@gmail.com>
2026-04-08 12:03:00 +0530

    Demo of basic working of redux using counter-app

---
+++ Week-9/08-04-2026/redux-demo/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App.jsx'
+import Counter from './features/counter/counter.jsx'
+import { store } from './app/store.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <>
+        <Counter />
+      </>
+    </Provider>
+  </StrictMode>,
+)
