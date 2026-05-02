8945481f6627cc97b745b43ab824c8b3b0a3f577
lakshay <lakshaymangal51@gmail.com>
2026-05-02 15:53:18 +0530

    Coursework and task for 2 May,2026

---
+++ Week-11/02-05-2026/EMS.InMemoryAPI/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi(options =>
+{
+    options.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0;
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
+{
+    app.MapOpenApi(); // Generates the OpenAPI document
+
+    // Adds the Swagger UI and points it to the built-in document endpoint
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "EMS API v1");
+    });
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Week-11/02-05-2026/EMS.InMemoryAPI/Repositories/EmployeeRepository.cs
@@ -0,0 +1,44 @@
+﻿using EMS.InMemoryAPI.Models;
+
+namespace EMS.InMemoryAPI.Repositories
+{
+    public class EmployeeRepository
+    {
+        public static readonly List<Employee> _employees = new(){
+
+        new Employee {Id=1, Name="Arjun",Department="HR", Email="abc@xyz.com", Salary=35000 },
+        new Employee {Id=2, Name="Bheem",Department="IT", Email="def@xyz.com", Salary=55000 }
+    };
+
+        public static List<Employee> GetAll() => _employees;
+
+        public static Employee? GetById (int id)=>_employees.FirstOrDefault(e=>e.Id==id);
+
+        public static void Add (Employee employee)
+        {
+            employee.Id= _employees.Max(e=>e.Id)+1;
+            _employees.Add(employee); 
+        }
+
+        public static bool Update (int id, Employee updatedEmployee)
+        {
+            var employee = GetById(id);
+            if (employee == null) return false;
+
+            employee.Name= updatedEmployee.Name;
+            employee.Department= updatedEmployee.Department;
+            employee.Email= updatedEmployee.Email;
+            employee.Salary =updatedEmployee.Salary;
+            return true;
+        }
+        public static bool Delete (int id)
+        {
+            var employee = GetById(id);
+            if(employee == null)  return false;
+
+            _employees.Remove(employee);
+            return true;
+        }
+
+    }
+}
