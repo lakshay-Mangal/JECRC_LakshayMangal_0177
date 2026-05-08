afc591dd44e3e766c4d051f434298930576ff6f0
lakshay <lakshaymangal51@gmail.com>
2026-05-08 14:54:27 +0530

    Assignment added

--- Week-8/11-04-2026/Weekend Assessment/MovieCatalogue/obj/Release/net10.0/MovieCatalogue.AssemblyInfo.cs
+++ Week-8/11-04-2026/Weekend Assessment/MovieCatalogue/obj/Release/net10.0/MovieCatalogue.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieCatalogue")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ce123939ed4c9ed046ecf76f979e626a3fcdb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieCatalogue")]
